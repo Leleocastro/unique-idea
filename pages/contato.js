eabd5acb449af69fede83f038b41e7afdb4a24fb
Leleocastro <hiperleo_castro@hotmail.com>
2021-03-25 12:53:57 -0300

    modificacoes 25/03 desk

--- pages/contato.js
+++ pages/contato.js
@@ -1,4 +1,5 @@
 import Menu from '../components/Menu.js';
+import Head from 'next/head'
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Spinner } from 'reactstrap';
@@ -6,6 +7,11 @@ import { Spinner } from 'reactstrap';
 function Contato(){
     return (
         <div>
+            <Head>
+                <title>Contato - L.Tag Developing</title>
+                <meta name='description' content='Contato par Desenvolvimento de Sites e Aplicativos'/>
+                <meta name='author' content='L.Tag - Leonardo Castro'/>
+            </Head>
             <Menu/>
             Contato: (61)9 8503-8552
             <div className="d-flex justify-content-center">
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,64 @@
 import Menu from '../components/Menu.js';
+import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {useState} from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { Jumbotron, Container } from 'reactstrap';
+import { useState } from 'react';
+library.add(fas);
 
 function Home() {
     return (
         <div>
+            <Head>
+                <title>Início - L.Tag Developing</title>
+                <meta name='description' content='Site de Desenvolvimento sobre Programação' />
+                <meta name='author' content='L.Tag - Leonardo Castro' />
+            </Head>
             <Menu />
-            <h4>Página Inicial em construção</h4>
-            <h1>Leonardo Castro</h1>
+            <Jumbotron fluid className="servicos">
+                <style>{`.servicos{
+                    padding-top: 80px;
+                    padding-bottom: 80px;
+                    background-color: #000;
+                    color: #fff;
+                    margin-bottom: 0rem !important;
+                }
+                .circulo{
+                    width: 140px;
+                    height: 140px;
+                    background-color: #fed136;
+                    font-size: 52px;
+                    padding-top: 24px;
+                }
+                .centralizar{
+                    margin: 0 auto !important;
+                    float: none !important;
+                }`}</style>
+                <Container className="text-center">
+                    <div>
+                        <h1 className="display-4">Página em Construção...</h1>
+                        <p className="lead pb-4">Temos a solução que a sua empresa precisa!</p>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div className="circulo centralizar rounded-circle">
+                                <FontAwesomeIcon icon="archive" />
+                            </div>
+                            <h2 className="mt-4 mb-4">Arquivamento</h2>
+                            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+                            <p><a className="btn btn-secondary" href="#" role="button">Saiba mais &raquo;</a></p>
+                        </div>
+                    </div>
+                </Container>
+            </Jumbotron>
             <Contador />
         </div>
     )
 }
 
-function Contador(){
+function Contador() {
     const [contador, setContador] = useState(1);
     function adicionarContador() {
         setContador(contador * 2);
@@ -27,4 +72,4 @@ function Contador(){
     )
 }
 
-export default Home
+export default Home;
