684533f6497a5d3a8eb8e0b7b780bd5920034e03
Leleocastro <hiperleo_castro@hotmail.com>
2021-03-26 00:34:28 -0300

    quase terminando

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../css/product.css'
+
+export default function MyApp({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
--- pages/contato.js
+++ pages/contato.js
@@ -2,21 +2,47 @@ import Menu from '../components/Menu.js';
 import Head from 'next/head'
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Spinner } from 'reactstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+// import { Spinner } from 'reactstrap';
+library.add(fas);
 
-function Contato(){
+function Contato() {
     return (
         <div>
             <Head>
                 <title>Contato - L.Tag Developing</title>
-                <meta name='description' content='Contato par Desenvolvimento de Sites e Aplicativos'/>
-                <meta name='author' content='L.Tag - Leonardo Castro'/>
+                <meta name='description' content='Contato par Desenvolvimento de Sites e Aplicativos' />
+                <meta name='author' content='L.Tag - Leonardo Castro' />
             </Head>
-            <Menu/>
-            Contato: (61)9 8503-8552
-            <div className="d-flex justify-content-center">
-                <Spinner color="dark" />
-            </div>
+            <Menu />
+            <style>
+                {`
+                .cover {
+                    padding: 0 1.5rem;
+                }
+                .cover .btn-lg {
+                    padding: .75rem 1.25rem;
+                    font-weight: 700;
+                }
+                .centralizar{
+                    margin: 0 auto !important;
+                    float: none !important;
+                `}
+            </style>
+            <body class="text-center">
+                <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+                    <main role="main" class="inner cover">
+                        <h1 class="cover-heading">Entre em contato.</h1>
+                            
+                        <p class="lead"><FontAwesomeIcon icon="phone-square" />  (61)9 8503-8552</p>
+                        <a href="tel:+5561985038552" class="btn btn-lg btn-secondary">Telefone</a>
+                        <p class="lead"><FontAwesomeIcon icon="envelope-open-text" />  leonardo.castro@ltag.com.br</p>
+                            <a href="mailto:leonardo.castro@ltag.com.br" class="btn btn-lg btn-secondary">E-mail</a>
+                    </main>
+                </div>
+            </body>
         </div>
     )
 }
